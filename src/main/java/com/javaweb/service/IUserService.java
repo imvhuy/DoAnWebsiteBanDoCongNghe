c41581e4bad6fbdc913e86c9930d2f589602a9e8
imvhuy <dangvanhuy1509@gmail.com>
2024-11-21 21:43:53 +0700

    merge from Thao

--- src/main/java/com/javaweb/service/IUserService.java
+++ src/main/java/com/javaweb/service/IUserService.java
@@ -5,11 +5,13 @@ import com.javaweb.model.UserModel;
 import org.apache.catalina.User;
 import org.eclipse.tags.shaded.org.apache.xpath.operations.Bool;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
 
+@Service
 public interface IUserService {
     UserModel findByUserNameAndStatus(String name, int status);
     UserModel findByUserName(String username);
